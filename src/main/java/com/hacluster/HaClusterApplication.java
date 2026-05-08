428dbf1a21665f7fa2cb8596b4d187475f4bcb68
jestavic <jestavic@gmail.com>
2026-05-08 22:43:52 +0530

    Add src/main/java/com/hacluster/HaClusterApplication.java

---
+++ src/main/java/com/hacluster/HaClusterApplication.java
@@ -0,0 +1,11 @@
+package com.hacluster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaClusterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HaClusterApplication.class, args);
+    }
+}
